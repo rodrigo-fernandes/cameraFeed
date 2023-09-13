c3b227521db008c409ef97cde38b662fae89645a
Rodrigo Fernandes <rodrigo.fernandes.1994@hotmail.com>
2023-09-13 18:55:55 -0300

    webcam

---
+++ script.js
@@ -0,0 +1,9 @@
+const cameraFeed = document.getElementById("cameraFeed");
+
+navigator.mediaDevices.getUserMedia({video:true}).then
+(stream=> {
+    cameraFeed.srcObject = stream;
+})
+.catch(error=>{
+    console.error("error accessing camera: ", error);
+})
